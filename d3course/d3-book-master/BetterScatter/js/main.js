0af16bc19ae0e4c0f97e51b7651d27734b07cedb
jgoodie <john.goodman@gmail.com>
2022-05-15 18:13:35 -0700

    more d3 stuff

---
+++ d3course/d3-book-master/BetterScatter/js/main.js
@@ -0,0 +1,119 @@
+// var dataset = [
+//     [ 5, 20],
+//     [480, 90],
+//     [250, 50],
+//     [100, 33],
+//     [330, 95],
+//     [410, 12],
+//     [475, 44],
+//     [25, 67],
+//     [ 85, 21],
+//     [220, 88],
+//     [600, 150]
+// ];
+
+//Dynamic, random dataset
+var dataset = [];
+var numDataPoints = 50;
+var xRange = Math.random() * 1000;
+var yRange = Math.random() * 1000;
+for (var i = 0; i < numDataPoints; i++) {
+    var newNumber1 = Math.floor(Math.random() * xRange); var newNumber2 = Math.floor(Math.random() * yRange); dataset.push([newNumber1, newNumber2]);
+}
+
+
+var w = 640;
+var h = 480;
+var padding = 40;
+
+//For converting strings to Dates
+var parseTime = d3.timeParse("%m/%d/%y");
+
+//For converting Dates to strings
+var formatTime = d3.timeFormat("%b %e");
+
+
+const xMin = d3.min(dataset, function(d) { return d[0]; })
+const xMax = d3.max(dataset, function(d) { return d[0]; })
+const yMin = d3.min(dataset, function(d) { return d[1]; })
+const yMax = d3.max(dataset, function(d) { return d[1]; })
+
+//Create scale functions
+xScale = d3.scaleLinear()
+    .domain([0,xMax])
+    .range([padding,w-padding*2]);
+xAxis = d3.axisBottom().scale(xScale)
+
+yScale = d3.scaleLinear()
+    .domain([yMin, yMax])
+    .range([h - padding, padding]);
+yAxis = d3.axisLeft().scale(yScale)
+
+rScale = d3.scaleLinear()
+    .domain([0, yMax])
+    .range([2,5]);
+
+cScale = d3.scaleLinear()
+    .domain([0, yMax])
+    .range([0,255]);
+
+//Define X axis
+xAxis = d3.axisBottom()
+    .scale(xScale)
+    .ticks(8);
+
+//Define Y axis
+yAxis = d3.axisLeft()
+    .scale(yScale)
+    .ticks(5);
+//Create SVG element
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+//Generate date labels first, so they are in back
+svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d) {
+        return  "("+ d[0] + "," + d[1] + ")";
+    })
+    .attr("x", function(d) {
+        return xScale(d[0]);
+    })
+    .attr("y", function(d) {
+        return yScale(d[1]);
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "11px")
+    .attr("fill", "#bbb");
+
+//Generate circles last, so they appear in front
+svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) {
+        return xScale(d[0]);
+    })
+    .attr("cy", function(d) {
+        return yScale(d[1]);
+    })
+    .attr("r", function(d){
+        return rScale(d[1])*2;
+    })
+    .attr('fill', function(d){
+        return "rgb(10,150, " + cScale(d[1]) + ")";
+    })
+
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + (h-padding) + ")")
+    .call(xAxis)
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis)
+
---
+++ d3course/d3-book-master/UpdatedD3BarChart/js/main.js
@@ -0,0 +1,113 @@
+var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+
+//Dynamic Bar Chart
+// var dataset = [];
+// for (var i=0; i<25; i++){
+//     var newNumber = Math.floor(Math.random() * 30 + 5);
+//     dataset.push(newNumber);
+// }
+
+var w = 600;
+var h = 250;
+
+var xScale = d3.scaleBand()
+    .domain(d3.range(dataset.length))
+    .rangeRound([0, w])
+    .paddingInner(0.05)
+
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, h]);
+
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", function(d, i){
+        return xScale(i);
+    })
+    .attr("y", function(d){
+        return h - yScale(d);
+    })
+    .attr("width", xScale.bandwidth())
+    .attr("height", function(d){
+        return yScale(d);
+    })
+    .attr('fill', function(d){
+        return "rgb(10,100, " + Math.round(d*10) + ")";
+    })
+
+svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d){
+        return d;
+    })
+    .attr("text-anchor", "middle")
+    .attr("x", function(d, i){
+        return xScale(i) + xScale.bandwidth() / 2;
+    })
+    .attr("y", function(d){
+        return h - yScale(d) + 14;
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "11px")
+    .attr("fill", "white");
+
+d3.select("body").on("click", function(){
+    // console.log("on click")
+    // alert("Hey, don't click that!");
+    //New values for dataset
+    var dataset = [];
+    for (var i=0; i<25; i++){
+        var newNumber = Math.floor(Math.random() * 25 + 2);
+        dataset.push(newNumber);
+    }
+
+    yScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset)])
+        .range([0, h]);
+
+    //Update all rects
+    svg.selectAll("rect")
+        .data(dataset)
+        .transition()
+        .delay(function(d, i) {
+            return i / dataset.length * 1000; })
+        .duration(1000)
+        .ease(d3.easeBounceOut)
+        .attr("y", function(d) {
+            return h - yScale(d);
+        })
+        .attr("height", function(d) {
+            return yScale(d);
+        })
+        .attr("fill", function(d) {
+            return "rgb(10,100, " + Math.round(d*10) + ")";
+        });
+
+    //Update all labels
+    svg.selectAll("text")
+        .data(dataset)
+        .transition()
+        .delay(function(d, i) {
+            return i / dataset.length * 1000; })
+        .duration(1000)
+        .ease(d3.easeBounceOut)
+        .text(function(d) {
+            return d;
+        })
+        .attr("x", function(d, i) {
+            return xScale(i) + xScale.bandwidth() / 2;
+        })
+        .attr("y", function(d) {
+            return h - yScale(d) + 14;
+        });
+
+})
